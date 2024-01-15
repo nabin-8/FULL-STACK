a55348cc9e8e663802c5672ea8fd2d9f75105a55
Nabin Acharya <nabinacharya740@gmail.com>
2024-01-15 06:39:33 +0545

    learning file added

---
+++ Learning/Tailwind CSS/vite-project/src/App.jsx
@@ -0,0 +1,98 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      {/* <div
+        className="flex justify-center items-center h-screen">
+        <h1 className='text-8xl font-bold text-pink-400'>
+          I'M Tailwindcss
+        </h1>
+      </div> */}
+      
+        <h1 className='Demo'>Tailwindcss font</h1>
+
+    <div>
+      {/* Tailwindcss Font ezample */}
+      {/* <h1 className='font-sans'>I am font-sans</h1>
+      <h1 className='font-serif'>I am font-serif</h1>
+      <h1 className='font-mono'>I am font-serif</h1> */}
+    </div>
+
+
+      <div>
+        {/* Tailwindcss Font-size ezample */}
+        {/* <h1 className='text-sm'>Font size example</h1>
+        <h1 className='text-lg'>Font size example</h1>
+        <h1 className='text-xl'>Font size example</h1>
+        <h1 className='text-2xl'>Font size example</h1>
+        <h1 className='text-4xl'>Font size example</h1>
+        <h1 className='text-6xl'>Font size example</h1>
+        <h1 className='text-8xl'>Font size example</h1>
+        <h1 className='text-9xl'>Font size example</h1> */}
+      </div>
+
+
+      <div>
+        {/* Italic and not italic
+          italic : -	font-style: italic;
+          not-italic : -	font-style: normal;
+         */}
+         {/* <h1 className='italic'>I am italic font</h1>
+        <h1 className='not-italic'>I am normal font</h1> */}
+      </div>
+
+
+        <div>
+        {/* 4. Font Weight : Controlling the font weight of an element. */}
+        {/* <h1 className='font-thin'>font-thin'</h1>
+        <h1 className='font-extralight'>font-extralight</h1>
+        <h1 className='font-light'>font-light</h1>
+        <h1 className='font-normal'>font-normal</h1>
+        <h1 className='font-medium'>font-medium</h1>
+        <h1 className='font-semibold'>font-medium</h1>
+        <h1 className='font-black'>font-medium</h1> */}
+
+        </div>
+
+      <div>
+      {/* 5. Text-Align: controlling the alignment of text. */}
+
+      {/* text-left */}
+      {/* <p className='text-left bg-blue-gray-800 mb-2 w-80 h-20'> */}
+        LEFT
+      {/* </p> */}
+      {/* text-center */}
+      {/* <p className='text-center bg-blue-gray-800 mb-2 w-80 h-20'> */}
+        CENTER
+      {/* </p> */}
+      {/* text-right */}
+      {/* <p className='text-right bg-blue-gray-800 mb-2 w-80 h-20'> */}
+        RIGHT
+      {/* </p> */}
+      {/* text-justify */}
+      {/* <p className='text-justify bg-blue-gray-800 mb-2 w-80 h-20'> */}
+        JUSTIFY The Text
+      {/* </p> */}
+      {/* text-start */}
+      {/* <p className='text-start bg-blue-gray-800 mb-2 w-80 h-20'> */}
+        START
+      {/* </p> */}
+      {/* text-end */}
+      {/* <p className='text-end bg-blue-gray-800 mb-2 w-80 h-20'> */}
+        END
+      {/* </p> */}
+
+      {/* CENTER  */}
+      <h1
+   className=' font-serif text-6xl italic font-bold text-center'>
+    TailwindCSS
+  </h1>
+    </div>
+
+
+    </div>
+  )
+}
+
+export default App
