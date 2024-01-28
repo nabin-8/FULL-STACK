e19d9b019d143163a547757550f9d86458374dd0
Nabin Acharya <nabinacharya740@gmail.com>
2024-01-28 22:12:55 +0545

    react rouner

---
+++ Learning/FRONTEND/reactCourse/07reactRouter/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <h1 className=' text-orange-500 text-3xl text-center'>Hellow React router</h1>
+    </>
+  )
+}
+
+export default App
