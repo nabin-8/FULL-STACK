50bb6cd05aff8cde024ef8e19db73c7f95bfe461
Nabin Acharya <nabinacharya740@gmail.com>
2024-01-29 19:53:56 +0545

    router

--- Learning/FRONTEND/reactCourse/07reactRouter/src/App.jsx
+++ Learning/FRONTEND/reactCourse/07reactRouter/src/App.jsx
@@ -2,13 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header/Header'
+import Home from './components/Home/Home'
+import Footer from './components/Footer/Footer'
 
 function App() {
 
 
   return (
     <>
-    <h1 className=' text-orange-500 text-3xl text-center'>Hellow React router</h1>
+    {/* <Header/>
+    <Home />
+    <Footer /> */}
     </>
   )
 }
