51d3c2a8feca6f33ddb987c4c05ff23ba01cc72c
nabin-8 <nabinacharya740@gmail.com>
2024-07-27 08:45:24 +0545

    fs modules

--- Learning/BACKEND/Node.js/5.FS_Module/2.callback_api/index.js
+++ Learning/BACKEND/Node.js/5.FS_Module/2.callback_api/index.js
@@ -1,7 +1,12 @@
 import { error } from "console"
 import * as fs from "fs"
 
-fs.mkdir("c:\\nodejs", (error) => {
+// fs.mkdir("c:\\nodejs", (error) => {
+//     if (error) throw error
+//     console.log("Directory Created..");
+// })
+
+fs.mkdir("c:\\nodejs", function namw(error) {
     if (error) throw error
     console.log("Directory Created..");
 })
